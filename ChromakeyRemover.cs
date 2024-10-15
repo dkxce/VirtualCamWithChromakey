70e2f28eb91061b8f1820803ef01a3c1300465d2
dkxce <25371256+dkxce@users.noreply.github.com>
2024-10-15 16:11:51 +0300

    Add files via upload

---
+++ ChromakeyRemover.cs
@@ -0,0 +1,133 @@
+﻿//
+// ChromakeyRemover (C#)
+// dkxce.ChromakeyRemover
+// v 0.1, 15.10.2024
+// https://github.com/dkxce
+// en,ru,1251,utf-8
+//
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace dkxce
+{
+    public class ChromakeyRemover
+    {
+        public enum Channel { Red, Green, Blue }
+
+        public static (int, int, byte[]) RemoveChromaKey2Bytes(Bitmap image, 
+            int velocity = 8, int threshold = 96,
+            Channel? channel = Channel.Green, Color? transparentColor = null, 
+            int? min_threshold = null)
+        {
+            float delta_threshold = min_threshold.HasValue ? 1.0f / ((float)threshold - (float)min_threshold) : 0.0f;
+
+            int width = image.Width; 
+            int height = image.Height;
+            byte[] rgba;
+            
+            // Get Bitmap Array
+            {
+                BitmapData iData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                int bytes = Math.Abs(iData.Stride) * image.Height;
+                rgba = new byte[bytes];
+                Marshal.Copy(iData.Scan0, rgba, 0, bytes);
+                image.UnlockBits(iData);
+            };
+
+            if (transparentColor.HasValue)
+            {
+                var pixels = Enumerable.Range(0, rgba.Length / 4).Select(x => new
+                {
+                    B = rgba[x * 4],
+                    G = rgba[(x * 4) + 1],
+                    R = rgba[(x * 4) + 2],
+                    A = rgba[(x * 4) + 3],
+                    MakeTransparent = new Action<float>((alpha) =>
+                    {
+                        if (alpha >= 1)
+                        {
+                            rgba[(x * 4)] = transparentColor.Value.B;
+                            rgba[(x * 4) + 1] = transparentColor.Value.G;
+                            rgba[(x * 4) + 2] = transparentColor.Value.R;
+                            rgba[(x * 4) + 3] = transparentColor.Value.A;
+                        }
+                        else
+                        {
+                            rgba[(x * 4)] = (byte)(transparentColor.Value.B * alpha + rgba[x * 4] * (1 - alpha));
+                            rgba[(x * 4) + 1] = (byte)(transparentColor.Value.G * alpha + rgba[(x * 4) + 1] * (1 - alpha));
+                            rgba[(x * 4) + 2] = (byte)(transparentColor.Value.R * alpha + rgba[(x * 4) + 2] * (1 - alpha));
+                            rgba[(x * 4) + 3] = (byte)(transparentColor.Value.A * alpha + rgba[(x * 4) + 3] * (1 - alpha));
+                        };
+                    })
+                });
+                pixels.AsParallel().ForAll(pixel =>
+                {
+                    byte max = Math.Max(Math.Max(pixel.R, pixel.G), pixel.B);
+                    byte min = Math.Min(Math.Min(pixel.R, pixel.G), pixel.B);
+                    byte pColor = pixel.G;
+                    if (channel.HasValue)
+                    {
+                        if (channel == Channel.Red) pColor = pixel.R;
+                        if (channel == Channel.Blue) pColor = pixel.B;
+                    };
+                    if (pColor != min && (pColor == max || max - pColor < velocity))
+                    {
+                        byte mm = (byte)(max - min);
+                        if (mm > threshold) pixel.MakeTransparent(1.0f);
+                        else if (min_threshold.HasValue && mm >= min_threshold) pixel.MakeTransparent((mm - min_threshold.Value) * delta_threshold);
+                    };
+                });
+            }
+            else
+            {
+                var pixels = Enumerable.Range(0, rgba.Length / 4).Select(x => new
+                {
+                    B = rgba[x * 4],
+                    G = rgba[(x * 4) + 1],
+                    R = rgba[(x * 4) + 2],
+                    A = rgba[(x * 4) + 3],
+                    //MakeTransparent = new Action(() => rgba[(x * 4) + 3] = 0),
+                    MakeTransparent = new Action<float>((a) => rgba[(x * 4) + 3] = (byte)((float)rgba[(x * 4) + 3] * a)) // or a/2 .. a/5
+                });
+                pixels.AsParallel().ForAll(pixel =>
+                {
+                    byte max = Math.Max(Math.Max(pixel.R, pixel.B), pixel.G);
+                    byte min = Math.Min(Math.Min(pixel.R, pixel.B), pixel.G);
+                    byte pColor = pixel.G;
+                    if (channel.HasValue)
+                    {
+                        if (channel == Channel.Red) pColor = pixel.R;
+                        if (channel == Channel.Blue) pColor = pixel.B;
+                    };
+                    if (pColor != min && (pColor == max || max - pColor < velocity))
+                    {
+                        byte mm = (byte)(max - min);
+                        if (mm > threshold) pixel.MakeTransparent(0.0f);
+                        else if (min_threshold.HasValue && mm >= min_threshold) pixel.MakeTransparent(1.0f - ((mm - min_threshold.Value) * delta_threshold));
+                    };
+                });
+            };
+
+            return (width, height, rgba);
+        }
+
+        public static Bitmap RemoveChromaKey2Bitmap(Bitmap image, 
+            int velocity = 8, int threshold = 96,
+            Channel? channel = Channel.Green, Color? transparentColor = null,
+            int? min_threshold = null)
+        {
+            int width, height;
+            byte[] rgba;
+
+            (width, height, rgba) = RemoveChromaKey2Bytes(image, velocity, threshold, channel, transparentColor, min_threshold);
+
+            Bitmap output = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            BitmapData oData = output.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(rgba, 0, oData.Scan0, rgba.Length);
+            output.UnlockBits(oData);
+            return output;
+        }
+    }
+}
---
+++ RealCamToVirtualCamRouter.cs
@@ -0,0 +1,174 @@
+﻿//
+// RealCamToVirtualCamRouter (C#)
+// dkxce.RealCamToVirtualCamRouter
+// v 0.1, 15.10.2024
+// https://github.com/dkxce
+// en,ru,1251,utf-8
+//
+
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+// using OpenCVSharp
+using OpenCvSharp; 
+
+// using SixLabors.ImageSharp
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace dkxce
+{
+    public static class RealCamToVirtualCamRouter
+    {
+        private static Bitmap buffer_frame = null;
+        private static Mutex buffer_mutex = new Mutex();
+
+        // Using:
+        //  https://github.com/webcamoid/akvirtualcamera
+        // Need Camera Settings:
+        //  AkVCamManager add-device "VirtualCamera0"
+        //  AkVCamManager set-description VirtualCamera0 "dkxce.RealCamToVirtualCamRouter"
+        //  AkVCamManager supported-formats --output
+        //  AkVCamManager add-format VirtualCamera0 RGB24 720 480 25
+        //  AkVCamManager add-format VirtualCamera0 RGB24 640 480 25        
+        //  AkVCamManager set-picture "C:\Disk2\Images\il_1588xN.4792548473_b52j_640x480.png"
+        //  AkVCamManager update
+        //  AkVCamManager devices
+
+        public static string akvcamman_path = @"C:\Program Files\AkVirtualCamera\x64\AkVCamManager.exe";
+        public static string background_image = @"C:\Disk2\Images\il_1588xN.4792548473_b52j_640x480.png";
+
+        public static bool chromakey_remove = true;
+        public static int chromakey_velocity = 8;    // recommended value: 8
+        public static int chromakey_treshold = 60;   // recommended value: 96
+        public static ChromakeyRemover.Channel chromakey_channel = ChromakeyRemover.Channel.Green;
+        
+        public static int virtualcam_fps = 25;
+        public static int virtualcam_num = 0;       
+        public static string virtualcam_name = "VirtualCamera0";
+
+        public static int default_width = 720;
+        public static int default_height = 480;
+        public static bool in_progress = false;
+
+        public static void Route()
+        {
+            in_progress = true;
+            using (var vid = new VideoCapture(virtualcam_num, VideoCaptureAPIs.DSHOW))
+            {
+                vid.Set(VideoCaptureProperties.FrameWidth, default_width);
+                vid.Set(VideoCaptureProperties.FrameHeight, default_height);
+
+                if (!vid.IsOpened())
+                {
+                    Console.WriteLine("Failed to open camera!");
+                    return;
+                };
+
+                bool firstRun = true;
+                while (in_progress)
+                {
+                    using (Mat frame = new Mat())
+                    {
+                        if (!vid.Read(frame))   break;
+
+                        Bitmap img;
+                        using (var ms = new MemoryStream(frame.ToBytes())) img = (Bitmap)Bitmap.FromStream(ms);
+
+                        if (firstRun)
+                        {
+                            LaunchThread(img.Width, img.Height);
+                            firstRun = false;
+                        };
+
+                        buffer_mutex.WaitOne();
+                        RealCamToVirtualCamRouter.buffer_frame = img;
+                        buffer_mutex.ReleaseMutex();                                             
+                    };
+                };
+            };
+        }
+
+        // Entry Point
+        private static void LaunchThread(int width, int height)
+        {
+            Thread thr = new Thread(() => RouteThread((object)(new int[] { width, height })));
+            thr.Start();
+        }
+
+        private static void RouteThread(object wihe)
+        {
+            int width = ((int[])wihe)[0];
+            int height = ((int[])wihe)[1];
+        
+            // Init AKVCamManager
+            ProcessStartInfo psi = new ProcessStartInfo(akvcamman_path, String.Format("stream --fps {0} {1} RGB24 {2} {3}", virtualcam_fps, virtualcam_name, width, height));
+            psi.RedirectStandardInput = true;
+            psi.UseShellExecute = false;
+            Process akvcamproc = Process.Start(psi);
+
+            // Init & Resize Background Image
+            SixLabors.ImageSharp.Image background = SixLabors.ImageSharp.Image.Load(background_image);
+            background.Mutate(i => i.Resize(width, height));
+
+            // Init Frame Rate
+            DateTime lastLoop = DateTime.MinValue;
+            double delay = 1000 / virtualcam_fps;
+
+            // Result Video Loop
+            while (in_progress)
+            {
+                if ((DateTime.UtcNow - lastLoop).TotalMilliseconds < delay) continue;                
+
+                // Read Buffer
+                buffer_mutex.WaitOne();
+                Bitmap img = buffer_frame;
+                buffer_frame = null;
+                buffer_mutex.ReleaseMutex();
+                if (img == null) continue;
+                lastLoop = DateTime.UtcNow;
+                
+                byte[] raw_data;
+                int imline = 0;
+
+                // Remove Background & Get Image Data
+                if (chromakey_remove)
+                {
+                    (width, height, raw_data) = ChromakeyRemover.RemoveChromaKey2Bytes(img, chromakey_velocity, chromakey_treshold, chromakey_channel);
+                }
+                else // Get Image Data
+                {
+                    width = img.Width;
+                    height = img.Height;
+                    BitmapData iData = img.LockBits(new System.Drawing.Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                    raw_data = new byte[Math.Abs(iData.Stride) * img.Height];
+                    Marshal.Copy(iData.Scan0, raw_data, 0, raw_data.Length);
+                    img.UnlockBits(iData);
+                };
+                img.Dispose();
+
+                // Get Result Frame
+                SixLabors.ImageSharp.Image<Rgb24> result_frame = background.CloneAs<Rgb24>();
+
+                // Add Overlay
+                SixLabors.ImageSharp.Image ovelay = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(raw_data, width, height);
+                result_frame.Mutate(i => i.DrawImage(ovelay, new SixLabors.ImageSharp.Rectangle(0, 0, ovelay.Width, ovelay.Height), 1f));
+                ovelay.Dispose();
+
+                // Get RAW Data
+                imline = result_frame.Width * 3;
+                raw_data = new byte[imline * result_frame.Height];
+                result_frame.CopyPixelDataTo(raw_data);
+
+                // Clear Mem
+                result_frame.Dispose();
+
+                // Send Horizontal Lines to Virtual Camera
+                for (int i = 0; i < raw_data.Length; i += imline)
+                    akvcamproc.StandardInput.BaseStream.Write(raw_data, i, imline);
+            };
+        }
+    }
+}

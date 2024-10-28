bbe8fee7dc01dcbe126171e1e6250ec4430c337d
dkxce <25371256+dkxce@users.noreply.github.com>
2024-10-28 10:01:21 +0300

    Add files via upload

--- ChromakeyRemover.cs
+++ ChromakeyRemover.cs
@@ -1,7 +1,7 @@
 ﻿//
 // ChromakeyRemover (C#)
 // dkxce.ChromakeyRemover
-// v 0.3, 24.10.2024
+// v 0.4, 28.10.2024
 // https://github.com/dkxce
 // en,ru,1251,utf-8
 //
@@ -1052,6 +1052,173 @@ namespace dkxce
                 Mode = ProcessMode.Full
             };
             string suffix = $"_(hsv{converter.Mode}_{converter.MinTreshold}_{converter.MaxTreshold})";
+            Bitmap source_image = (Bitmap)Bitmap.FromFile(Path.Combine(current_path, "greenbox.jpg"));
+
+            using (Bitmap destination_image = converter.RemoveChromaKey2Bitmap(source_image))
+                destination_image.Save(Path.Combine(current_path, $"greenbox_color_replace{suffix}.jpg"));
+
+
+            converter.FullMask = false;
+            converter.TransparentColor = Color.Transparent;
+            using (Bitmap background_image = (Bitmap)Bitmap.FromFile(Path.Combine(current_path, "background.jpg")))
+            using (Bitmap overlayed_image = new Bitmap(background_image.Width, background_image.Height))
+            using (Graphics g = Graphics.FromImage(overlayed_image))
+            {
+                g.DrawImage(background_image, new Rectangle(0, 0, background_image.Width, background_image.Height));
+
+                using (Bitmap destination_image = converter.RemoveChromaKey2Bitmap(source_image))
+                    g.DrawImage(destination_image, 0, 200, new Rectangle(0, 200, background_image.Width, background_image.Height), GraphicsUnit.Pixel);
+
+                overlayed_image.Save(Path.Combine(current_path, $"greenbox_with_background{suffix}.jpg"));
+            };
+
+            source_image.Dispose();
+        }
+    }
+
+    // Multiple YCbCr Colors with same treshold
+    public class YCbCrMultiChromakeyRemover
+    {
+        private Color[] base_colors = new Color[] { Color.Green };
+        private Color? transparent_color = null;
+        private bool full_mask = false;
+        private int[][] mask_colors = new int[][] { new int[] { 150, 44, 21 } };
+        private byte min_treshold = 8;
+        private byte max_treshold = 96;
+
+        public Color[] MaskColors
+        {
+            set
+            {
+                base_colors = value;
+                if (base_colors == null || base_colors.Length == 0) base_colors = new Color[] { Color.Green };
+                mask_colors = new int[base_colors.Length][];
+                for(int i = 0; i< base_colors.Length; i++)
+                    mask_colors[i] = new int[] { rgb2y(base_colors[i].R, base_colors[i].G, base_colors[i].B), rgb2cb(base_colors[i].R, base_colors[i].G, base_colors[i].B), rgb2cr(base_colors[i].R, base_colors[i].G, base_colors[i].B) };
+            }
+            get
+            {
+                return (Color[])base_colors.Clone();
+            }
+        }
+        public Color? TransparentColor { set { transparent_color = value; } get { return transparent_color; } }
+        public bool FullMask { set { full_mask = value; } get { return full_mask; } }
+        public byte MinTreshold { set { min_treshold = value; } get { return min_treshold; } }
+        public byte MaxTreshold { set { max_treshold = value; } get { return max_treshold; } }
+
+
+        private static int rgb2y(int r, int g, int b) => (int)Math.Round(0.299 * (double)r + 0.587 * (double)g + 0.114 * (double)b);
+
+        private static int rgb2cb(int r, int g, int b) => (int)Math.Round(128.0 + -0.168736 * (double)r - 0.331264 * (double)g + 0.5 * (double)b);
+
+        private static int rgb2cr(int r, int g, int b) => (int)Math.Round(128.0 + 0.5 * (double)r - 0.418688 * (double)g - 0.081312 * (double)b);
+
+
+        private double colorclose(int R, int G, int B, out int OR, out int OG, out int OB)
+        {
+            OR = R; OG = G; OB = B;
+            double max_dist = double.MaxValue;
+            for(int i=0;i<mask_colors.Length;i++)
+            {
+                double d = Math.Sqrt(Math.Pow(mask_colors[i][1] - rgb2cb(R, G, B), 2) + Math.Pow(mask_colors[i][2] - rgb2cr(R, G, B), 2));
+                if (d < max_dist)
+                {
+                    max_dist = d;
+                    OR = R;
+                    OG = G;
+                    OB = B;
+                };
+            };
+            
+            return 1 - (max_dist < min_treshold ? 0 : max_dist > max_treshold ? 1 : (max_dist - min_treshold) / (max_treshold - min_treshold));
+        }
+
+
+        public (int, int, byte[]) RemoveChromaKey2Bytes(Bitmap image)
+        {
+            int width = image.Width;
+            int height = image.Height;
+            byte[] rgba_foreground;
+
+            // Get Bitmap Array
+            {
+                BitmapData iData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                int bytes = Math.Abs(iData.Stride) * image.Height;
+                rgba_foreground = new byte[bytes];
+                Marshal.Copy(iData.Scan0, rgba_foreground, 0, bytes);
+                image.UnlockBits(iData);
+            };
+
+            if (transparent_color.HasValue)
+            {
+                var pixels = Enumerable.Range(0, rgba_foreground.Length / 4).Select(x => new
+                {
+                    B = rgba_foreground[(x * 4)],
+                    G = rgba_foreground[(x * 4) + 1],
+                    R = rgba_foreground[(x * 4) + 2],
+                    MakeTransparent = new Action<double, int, int, int>((mask, R, G, B) =>
+                    {
+                        if (full_mask)
+                        {
+                            rgba_foreground[(x * 4)] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4)] - mask * B, 0) + mask * transparent_color.Value.B), 255); //B
+                            rgba_foreground[(x * 4) + 1] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4) + 1] - mask * G, 0) + mask * transparent_color.Value.G), 255); //G
+                            rgba_foreground[(x * 4) + 2] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4) + 2] - mask * R, 0) + mask * transparent_color.Value.R), 255); //R
+                        };
+                        rgba_foreground[(x * 4) + 3] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4) + 3] - mask * 255, 0) + mask * transparent_color.Value.A), 255);     //A
+                    })
+                });
+                pixels.AsParallel().ForAll(pixel => pixel.MakeTransparent(colorclose(pixel.R, pixel.G, pixel.B, out int OR, out int OG, out int OB), OR, OG, OB));
+            }
+            else
+            {
+                var pixels = Enumerable.Range(0, rgba_foreground.Length / 4).Select(x => new
+                {
+                    B = rgba_foreground[(x * 4)],
+                    G = rgba_foreground[(x * 4) + 1],
+                    R = rgba_foreground[(x * 4) + 2],
+                    MakeTransparent = new Action<double,int,int,int>((mask, R, G, B) =>
+                    {
+                        if (full_mask)
+                        {
+                            rgba_foreground[(x * 4)] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4)] - mask * B, 0) + mask * 0), 255); //B
+                            rgba_foreground[(x * 4) + 1] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4) + 1] - mask * G, 0) + mask * 0), 255); //G
+                            rgba_foreground[(x * 4) + 2] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4) + 2] - mask * R, 0) + mask * 0), 255); //R
+                        };
+                        rgba_foreground[(x * 4) + 3] = (byte)Math.Min((Math.Max(rgba_foreground[(x * 4) + 3] - mask * 255, 0) + mask * 0), 255);               //A
+                    })
+                });
+                pixels.AsParallel().ForAll(pixel => pixel.MakeTransparent(colorclose(pixel.R, pixel.G, pixel.B, out int OR, out int OG, out int OB), OR, OG, OB));
+            };
+
+            return (width, height, rgba_foreground);
+        }
+
+        public Bitmap RemoveChromaKey2Bitmap(Bitmap image)
+        {
+            int width, height;
+            byte[] rgba;
+
+            (width, height, rgba) = RemoveChromaKey2Bytes(image);
+
+            Bitmap output = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            BitmapData oData = output.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(rgba, 0, oData.Scan0, rgba.Length);
+            output.UnlockBits(oData);
+            return output;
+        }
+
+        public static void Test()
+        {
+            string current_path = Directory.GetCurrentDirectory();
+            YCbCrMultiChromakeyRemover converter = new YCbCrMultiChromakeyRemover()
+            {
+                FullMask = true,
+                MaskColors = new Color[] { Color.FromArgb(21, 238, 211), Color.FromArgb(255, 38, 40) },
+                TransparentColor = Color.Fuchsia,
+                MinTreshold = 50,
+                MaxTreshold = 82
+            };
+            string suffix = $"_(ycbcrm_{converter.MinTreshold}_{converter.MaxTreshold})";
             Bitmap source_image = (Bitmap)Bitmap.FromFile(Path.Combine(current_path, "greenbox.jpg"));
 
             using (Bitmap destination_image = converter.RemoveChromaKey2Bitmap(source_image))
